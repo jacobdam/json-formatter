718c403cd008b5bce1d0280add38b6a1aae081d5
Jacob Dam <jdam@digication.com>
2013-08-31 16:31:07 +0700

    Add test

---
+++ test/json_formatter_test.js
@@ -0,0 +1,20 @@
+var jsonFormatter = require('../lib/entry.js');
+
+module.exports = {
+  testFormatJSON: function(test){
+    var input = '{"a":[1,2,null,{}],"b":true}';
+    var expectedOutput = [
+      '{', '\n',
+      '    ', '"a"', ':', ' ', '[', '\n',
+      '    ', '    ', '1', ',', '\n',
+      '    ', '    ', '2', ',', '\n',
+      '    ', '    ', 'null', ',', '\n',
+      '    ', '    ', '{', ' ', '}', '\n',
+      '    ', ']', ',', '\n',
+      '    ', '"b"', ':', ' ', 'true', '\n',
+      '}'
+    ].join('')
+    test.equal(jsonFormatter.formatJSON(input), expectedOutput);
+    test.done();
+  }
+};
